018c5158ca18f63baf1af1ada006886c62e5ec4f
Robertextex <robertgrimba@gmail.com>
2024-08-18 23:36:25 -0400

    practica2

---
+++ practica-2/src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Acerca de la Pagina</h1>
+  </div>
+</template>
---
+++ practica-2/src/views/Singup.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+      <img src="@/assets/logo.png" class="logoImg rounded d-block mx-auto" />
+      <SingUp />
+    </div>
+</template>
+  
+<script>
+  // @ is an alias to /src
+  import SingUp from "@/components/singUp/singUp.vue";
+    import SignUp from "@/components/singUp/singUp.vue";
+  export default {
+    name: "SingUp",
+    components: {
+      singUp,
+    },
+  };
+</script>
